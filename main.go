14d5207e2a994b01643c1e26aa38c057d05982bb
Phanurat <paunavt5@gmail.com>
2024-01-17 11:05:06 +0700

    Basic Go lang

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
+/*func main() {
 	fmt.Println("Hello Go Programming")
+}*/
+
+// นิยามตัวแปร
+func main() {
+	var name string = "Phanurat"
+	var age int = 25
+	var score float32 = 95.8
+	var isPass bool = true
+
+	fmt.Println("My name is :", name)
+	fmt.Println("Age :", age)
+	fmt.Println("Score :", score)
+	fmt.Println("Pass Exam :", isPass)
 }
